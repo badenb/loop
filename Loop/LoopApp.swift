8357015b0b353a0d098bef87454f9ab1a586b025
Baden <baden1998@gmail.com>
2025-07-15 06:46:48 -0500

    Initial Commit

---
+++ Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by Baden Bennett on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoopTests/LoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoopTests.swift
+//  LoopTests
+//
+//  Created by Baden Bennett on 7/15/25.
+//
+
+import Testing
+@testable import Loop
+
+struct LoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
